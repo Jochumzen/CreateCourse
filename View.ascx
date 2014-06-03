36687fba3dd14b2ac2d247d2693b5b676305fb8e
Jochumzen <peter.jochumzen@nek.lu.se>
2014-06-03 08:19:52 +0200

    first commit

---
+++ View.ascx
@@ -0,0 +1,2 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="View.ascx.cs" Inherits="Plugghest.Modules.CreateCourse.View" %>
+<asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
---
+++ View.ascx.cs
@@ -0,0 +1,92 @@
+﻿/*
+' Copyright (c) 2014  Plugghest.com
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using System;
+using System.Web.UI.WebControls;
+using Plugghest.Modules.CreateCourse.Components;
+using DotNetNuke.Security;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Modules.Actions;
+using DotNetNuke.Services.Localization;
+using DotNetNuke.UI.Utilities;
+using Plugghest.Base2;
+
+namespace Plugghest.Modules.CreateCourse
+{
+    /// -----------------------------------------------------------------------------
+    /// <summary>
+    /// The View class displays the content
+    /// 
+    /// Typically your view control would be used to display content or functionality in your module.
+    /// 
+    /// View may be the only control you have in your project depending on the complexity of your module
+    /// 
+    /// Because the control inherits from CreateCourseModuleBase you have access to any custom properties
+    /// defined there, as well as properties from DNN such as PortalId, ModuleId, TabId, UserId and many more.
+    /// 
+    /// </summary>
+    /// -----------------------------------------------------------------------------
+    public partial class View : CreateCourseModuleBase, IActionable
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+            }
+            catch (Exception exc) //Module failed to load
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+
+        public ModuleActionCollection ModuleActions
+        {
+            get
+            {
+                var actions = new ModuleActionCollection
+                    {
+                        {
+                            GetNextActionID(), Localization.GetString("EditModule", LocalResourceFile), "", "", "",
+                            EditUrl(), false, SecurityAccessLevel.Edit, true, false
+                        }
+                    };
+                return actions;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            BaseHandler bh = new BaseHandler();
+            CourseContainer cc = new CourseContainer(new Localization().CurrentCulture);
+
+            //Read SubjectId from QueryString (0 if no QS)
+            string subjectStr = Page.Request.QueryString["s"];
+            if (subjectStr != null)
+            {
+                int subjectId;
+                bool isNum = int.TryParse(subjectStr, out subjectId);
+                if (isNum)
+                    cc.TheCourse.SubjectId = subjectId;
+            }
+
+            cc.TheCourse.CreatedByUserId = this.UserId;
+            cc.TheCourse.WhoCanEdit = EWhoCanEdit.NotSet;
+            cc.SetTitle("Title");
+            cc.SetDescription("Description");
+            bh.CreateCourse(cc);
+            Response.Redirect(DotNetNuke.Common.Globals.NavigateURL(cc.TheCourse.TabId, "", ""));
+
+        }
+    }
+}
